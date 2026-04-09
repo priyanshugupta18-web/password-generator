f723f5cf081b5cb95208882e256b785fcb1dd641
Priyanshu <priyanshuguptawebdev@gmail.com>
2026-04-10 00:29:11 +0530

    Created the complete logic except the copy button

---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+let charCountInput = document.querySelector("#char-count");
+let numCountInput = document.querySelector("#num-count");
+let specCountInput = document.querySelector("#spec-count");
+let generateBtn = document.querySelector("#generate-btn");
+
+let result = document.querySelector("#result");
+
+let genPass = (e) => {
+    e.preventDefault();
+
+    let charCount = parseInt(charCountInput.value);
+    let numCount = parseInt(numCountInput.value);
+    let specCount = parseInt(specCountInput.value);
+
+    let alphaCount = charCount - (numCount + specCount);
+
+    if (alphaCount < 0) {
+        alert("Total characters must be >= numbers + special characters");
+        return;
+    }
+
+    if (isNaN(charCount) || isNaN(numCount) || isNaN(specCount)) {
+        alert("Please enter valid numbers");
+        return;
+    }
+
+    let key1 = genAlpha(alphaCount);
+    let key2 = genNum(numCount);
+    let key3 = genSpec(specCount);
+
+    let password = key1 + key2 + key3;
+
+    result.value = password;
+}
+
+generateBtn.addEventListener("click", genPass);
+
+let genAlpha = (alphaCount) => {
+    let alphaArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    let alphaPass = "";
+    let alphaChar = "";
+
+    for (let i = 0; i < alphaCount; i++) {
+        alphaChar = alphaArr[Math.floor(alphaArr.length * Math.random())];
+        alphaPass = alphaPass + alphaChar;
+    }
+
+    return alphaPass;
+}
+
+let genNum = (numCount) => {
+    let numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let numPass = "";
+    let numChar = "";
+
+    for (let j = 0; j < numCount; j++) {
+        numChar = numArr[Math.floor(numArr.length * Math.random())];
+        numPass = numPass + numChar;
+    }
+
+    return numPass;
+}
+
+let genSpec = (specCount) => {
+    let specArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", ";", ":", "'", "\"", "<", ">", ",", ".", "?", "/", "`", "~"];
+    let specPass = "";
+    let specChar = "";
+    for (let k = 0; k < specCount; k++) {
+        specChar = specArr[Math.floor(specArr.length * Math.random())];
+
+        specPass = specPass + specChar;
+    }
+
+    return specPass;
+}
